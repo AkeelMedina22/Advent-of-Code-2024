e82d0fad471bb01faf903b89f0592c728a1a5563
AkeelMedina22 <akeelmedina22@gmail.com>
2024-12-02 16:33:25 +0500

    d1 formatting

--- Day 1/solution.py
+++ Day 1/solution.py
@@ -1,24 +1,20 @@
 import numpy as np
-from functools import partial
-from typing import Dict, Tuple, List 
 
 def task_p1(data: np.array) -> int:
     
     data.sort(axis=1)
-
     return np.sum(np.abs(data[0:,] - data[-1:,]))
-
-def task_p2(data: np.array) -> int:
-    
-    freq = dict(zip(*np.unique(data[1], return_counts=True)))
-    return np.sum([i*freq[i] if i in freq else 0 for i in data[0]])
-
 
 def part_1() -> None:
     with open("data/data.txt", "r") as f:
         data = np.array([tuple(map(int, line.split())) for line in f]).T
 
     print(task_p1(data))
+
+def task_p2(data: np.array) -> int:
+    
+    freq = dict(zip(*np.unique(data[1], return_counts=True)))
+    return np.sum([i*freq[i] if i in freq else 0 for i in data[0]])
 
 def part_2() -> None:
     with open("data/data.txt", "r") as f:
